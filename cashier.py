09cdfa9e787f9aa6ee9a2879d306267b7cbba189
rizkiajimahardika <rizkimahardika37@gmail.com>
2023-01-15 15:06:24 +0700

    first commit

---
+++ cashier.py
@@ -0,0 +1,10 @@
+from modules import transaction
+      
+'''Test the program with a test_case'''   
+test_case = transaction.Transaction()
+test_case.add_item(['Ayam Goreng', 2, 220000])
+test_case.add_item(['Pasta Gigi', 3, 150000])
+test_case.delete_item('Pasta Gigi')
+test_case.total_price()
+test_case.reset_transaction()
+
